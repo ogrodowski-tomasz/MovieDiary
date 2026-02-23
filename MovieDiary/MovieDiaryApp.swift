b61e4975e119698711ef35e5f0d3bc710f173344
Tomasz Ogrodowski <tomasz.ogrodowski@itcraft.pl>
2026-02-23 13:29:28 +0100

    Initial Commit

---
+++ MovieDiary/MovieDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDiaryApp.swift
+//  MovieDiary
+//
+//  Created by itcraft on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
